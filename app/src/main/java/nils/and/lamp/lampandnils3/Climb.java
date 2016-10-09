f8f5e2d22c626b6f603e8fb5a321cc6e5f5ce561
Joe Duffin <joey.duffin@gmail.com>
2016-10-09 13:13:51 +0100

    added climb object

---
+++ app/src/main/java/nils/and/lamp/lampandnils3/Climb.java
@@ -0,0 +1,67 @@
+package nils.and.lamp.lampandnils3;
+
+import android.graphics.drawable.BitmapDrawable;
+
+/**
+ * Created by joe on 09/10/16.
+ */
+
+public class Climb {
+
+    private BitmapDrawable photo;
+    private String name;
+    private String grade;
+    private String length;
+    private String description;
+
+    public Climb(BitmapDrawable photo, String name, String grade, String length, String description) {
+        this.photo = photo;
+        this.name = name;
+        this.grade = grade;
+        this.length = length;
+        this.description = description;
+    }
+
+    public Climb() {
+    }
+
+    public BitmapDrawable getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(BitmapDrawable photo) {
+        this.photo = photo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public void setLength(String length) {
+        this.length = length;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
